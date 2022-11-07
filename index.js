88294d35569eabc0cbd9c59f179e3ff05a70d60d
Rayandson <rayandson.silva321@gmail.com>
2022-11-07 15:11:06 -0300

    Mostrar tweets de um usuario

--- index.js
+++ index.js
@@ -10,20 +10,28 @@ const tweets = []
 
 app.post('/sign-up', (req, res) => {
 const {username, avatar} = req.body;
+if(!username || !avatar) {
+    res.status(400).send("Todos os campos são obrigatórios")
+    return
+}
 const id = users.length + 1;
 const newUser = {id: id, username: username, avatar: avatar}
  users.push(newUser)
-res.send("OK")
+ res.status(201).send("OK")
 })
 
 
 app.post('/tweets', (req, res) => {
     const {username, tweet} = req.body;
+    if(!username || !tweet) {
+        res.status(400).send("Todos os campos são obrigatórios")
+        return
+    }
     const id = tweets.length + 1;
     const user = users.find((obj) => obj.username === username)
     const newTweet = {id: id, username: username, avatar: user.avatar, tweet: tweet}
      tweets.push(newTweet)
-    res.send("OK")
+    res.status(201).send("OK")
     })
 
 app.get('/tweets', (req, res) => {
@@ -40,7 +48,14 @@ app.get('/tweets', (req, res) => {
     res.send(tweetsToSend)
 })
 
-
+app.get('/tweets/:username', (req, res) => {
+    const userTweets = tweets.filter((t) => (req.params.username === t.username))
+    if(userTweets.length === 0) {
+        res.status(404).send("Usuário não encontrado")
+    } else {
+        res.send(userTweets)
+    }
+})
 
 
 app.listen(5000)

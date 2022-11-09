4bfb544604d19390216b1ce54c380b0c7256a5d0
Rayandson <rayandson.silva321@gmail.com>
2022-11-09 00:34:52 -0300

    integraçao com banco de dados

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from 'express'
 import cors from 'cors'
+import { MongoClient } from 'mongodb';
 
 const app = express();
 app.use(cors())
 app.use(express.json())
+const mongoClient = new MongoClient("mongodb://localhost:27017");
+let db;
 
-const users = []
-const tweets = []
+const users = [];
+const tweets  = []
+
+mongoClient.connect().then(() => {
+    db = mongoClient.db("tweterooBD")
+})
 
 app.post('/sign-up', (req, res) => {
 const {username, avatar} = req.body;
@@ -14,48 +21,44 @@ if(!username || !avatar) {
     res.status(400).send("Todos os campos são obrigatórios")
     return
 }
-const id = users.length + 1;
-const newUser = {id: id, username: username, avatar: avatar}
- users.push(newUser)
- res.status(201).send("OK")
+const newUser = {username: username, avatar: avatar}
+db.collection("users").insert(newUser).then((response) => {
+    res.status(201).send("OK")
+})
 })
 
 
 app.post('/tweets', (req, res) => {
-    const {username, tweet} = req.body;
+    const {username, avatar, tweet} = req.body;
     if(!username || !tweet) {
         res.status(400).send("Todos os campos são obrigatórios")
         return
     }
-    const id = tweets.length + 1;
-    const user = users.find((obj) => obj.username === username)
-    const newTweet = {id: id, username: username, avatar: user.avatar, tweet: tweet}
-     tweets.push(newTweet)
-    res.status(201).send("OK")
+    // const user = users.find((obj) => obj.username === username)
+    
+    const newTweet = {username: username, avatar: avatar, tweet: tweet}
+     db.collection("tweets").insert(newTweet).then(() => {
+        res.status(201).send("OK")
+     })
     })
 
 app.get('/tweets', (req, res) => {
-    const tweetsToSend = []
-    if(tweets.length <= 10) {
-        for(let i=0; i < tweets.length; i++) {
-            tweetsToSend.push(tweets[i])
-        }
-    } else {
-        for(let i=0; i < 10; i++) {
-            tweetsToSend.push(tweets[i])
-        } 
-    }
-    res.send(tweetsToSend)
+    db.collection("tweets").find().toArray().then( tweets => {
+        console.log(tweets)
+        res.send(tweets.slice(-10).reverse())
+    })
 })
 
-app.get('/tweets/:username', (req, res) => {
-    const userTweets = tweets.filter((t) => (req.params.username === t.username))
-    if(userTweets.length === 0) {
-        res.status(404).send("Usuário não encontrado")
-    } else {
-        res.send(userTweets)
-    }
-})
+// app.get('/tweets/:username', (req, res) => {
+//     db.collection("tweets").find().toArray().then( tweets => {
+//     const userTweets = tweets.filter((t) => (req.params.username === t.username))
+//     if(userTweets.length === 0) {
+//         res.status(404).send("Usuário não encontrado")
+//     } else {
+//         res.send(userTweets)
+//     }
+// })
+// })
 
 
 app.listen(5000)
